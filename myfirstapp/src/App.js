3db613506914ea34f014b94566610247d5b5ed06
IORD1 <pratham111ingole@gmail.com>
2023-03-01 13:09:27 +0530

    created first app

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import shar from './shar.png';
+import './App.css';
+
+
+
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={shar} className="App-logo" alt="logo" /> */}
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload. 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React with pratham
+        </a>
+    
+        <button>Click me</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
